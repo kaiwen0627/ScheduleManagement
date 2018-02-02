099441262e013cb539a16f9eeca7c8bcd0bfbe78
kaiweno425 <kaiwen0425@163.com>
2018-02-02 14:16:16 +0800

    0.5
    kaiwen index

--- js/Schedule.js
+++ js/Schedule.js
@@ -1,29 +1,16 @@
 $(function () {
     //点击返回
-    $('.goBack').on('click', function () {
-        console.log('999');
+    $('.goBack').on('click', function () {       
         window.location.href = '/index.html';
     });
     //选择日期
-    (function () {
-        var i = 1;
-        $('.navInfo').on('click', function () {
-            if(i == 1){
-                $('.dateMenu').slideDown(); 
-                changeData();
-                i = -i;
-            } else {
-                $('.dateMenu').slideUp(); 
-                i = -i;
-            }       
-        })
-        function changeData() {
-            $('.dateMenu').on('click','li', function () {
-                $('.navInfo').text($(this).text());
-                $('.dateMenu').hide();
-                //在此处写数据刷新操作
-                alert('读数据库')
-            })
-       }
-  })()
+    $('.navInfo').on('click', function () {
+        $('.dateMenu').slideDown();
+    });
+    $('.dateMenu').on('click', 'li', function () {
+        $('.navInfo').text($(this).text());       
+        $('.dateMenu').slideUp();
+        alert('读数据库')
+    });
+
 })
