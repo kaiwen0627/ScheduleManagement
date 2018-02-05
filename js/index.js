bc4dec45085e3ab2d07dc40feb338ab15c16204e
kaiweno425 <kaiwen0425@163.com>
2018-02-05 13:27:14 +0800

    1.0
    合并

--- js/index.js
+++ js/index.js
@@ -1,29 +1,40 @@
-var mySchedule = new Schedule({
-    el: '#schedule-box',
-    //date: '2018-9-20',
-    clickCb: function (y, m, d) {
-        document
-            .querySelector('#h3Ele')
-            .innerHTML = '日期：' + y + '-' + m + '-' + d
-    },
-    nextMonthCb: function (y, m, d) {
-        document
-            .querySelector('#h3Ele')
-            .innerHTML = '日期：' + y + '-' + m + '-' + d
-    },
-    nextYeayCb: function (y, m, d) {
-        document
-            .querySelector('#h3Ele')
-            .innerHTML = '日期：' + y + '-' + m + '-' + d
-    },
-    prevMonthCb: function (y, m, d) {
-        document
-            .querySelector('#h3Ele')
-            .innerHTML = '日期：' + y + '-' + m + '-' + d
-    },
-    prevYearCb: function (y, m, d) {
-        document
-            .querySelector('#h3Ele')
-            .innerHTML = '日期：' + y + '-' + m + '-' + d
-    }
-});
+function data() {
+    console.log('ppp');
+    var mySchedule = new Schedule({
+        el: '#schedule-box',
+        //date: '2018-9-20',
+        clickCb: function (y, m, d) {
+            console.log(y, m, d);
+        },
+        nextMonthCb: function (y, m, d) {
+            console.log(y, m, d);
+        },
+        nextYeayCb: function (y, m, d) {
+            console.log(y, m, d);
+        },
+        prevMonthCb: function (y, m, d) {
+            console.log(y, m, d);
+        },
+        prevYearCb: function (y, m, d) {
+            console.log(y, m, d);
+        }
+    });
+};
+data();
+
+//切换视图
+$('.views-btn .otherView').on('click', function () {
+    window.location = './Schedule.html';
+})
+
+$('.addNew').on('click', function () {
+    window.location = './creatWork.html';
+})
+
+$('.mainList').on('click', 'li', function () {
+    window.location = './listinfo.html?';
+})
+
+$('.views-btn .gotoday').on('click', function () {
+    data();
+})
